d6eefd0563e3ad6ea8b24b81d89bb91d72d325b7
Victor Vasiloi <victor.vasiloi@gmail.com>
2020-11-23 17:38:52 +0200

    add add_to_cart event

---
+++ tests/Event/AddToCartEventTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\GoogleAnalyticsMeasurementProtocol\Event;
+
+use PHPUnit\Framework\TestCase;
+
+class AddToCartEventTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_returns_array(): void
+    {
+        $event = new AddAddToCartEvent();
+        $event->parameters->currency = 'USD';
+        $event->parameters->value = 123.95;
+
+        $item = new GenericItemEventParameters();
+        $item->itemId = 'GUCCI_BAG_1234';
+
+        $event->parameters->addItem($item);
+
+        self::assertSame([
+            'name' => 'add_to_cart',
+            'params' => [
+                'currency' => 'USD',
+                'value' => 123.95,
+                'items' => [
+                    ['item_id' => 'GUCCI_BAG_1234'],
+                ],
+            ],
+        ], $event->toArray());
+    }
+}
