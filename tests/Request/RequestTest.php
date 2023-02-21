0f8b169ec39092d9043d1989ec8ab2ba0bc860a0
Joachim Løvgaard <joachim@loevgaard.dk>
2023-02-21 11:21:15 +0100

    Remove references to HasSetters trait

--- tests/Request/RequestTest.php
+++ tests/Request/RequestTest.php
@@ -9,7 +9,6 @@ use Setono\GoogleAnalyticsMeasurementProtocol\Request\Body\Body;
 
 /**
  * @covers \Setono\GoogleAnalyticsMeasurementProtocol\Request\Request
- * @covers \Setono\GoogleAnalyticsMeasurementProtocol\Request\HasSetters
  */
 final class RequestTest extends TestCase
 {
@@ -24,26 +23,5 @@ final class RequestTest extends TestCase
         self::assertSame('API_SECRET', $request->getApiSecret());
         self::assertSame('G-12341234', $request->getMeasurementId());
         self::assertSame($body, $request->getBody());
-    }
-
-    /**
-     * This test will test the HasSetters trait
-     *
-     * @test
-     */
-    public function it_is_immutable(): void
-    {
-        $request = new Request('API_SECRET', 'G-12341234', Body::create('CLIENT_ID'));
-
-        $newRequest = $request->setApiSecret('NEW_API_SECRET')
-            ->setMeasurementId('G-56785678')
-        ;
-
-        // test that the original $request has the same values
-        self::assertSame('NEW_API_SECRET', $request->getApiSecret());
-        self::assertSame('G-56785678', $request->getMeasurementId());
-
-        // test that the new object IS the same as the old object
-        self::assertSame($request, $newRequest);
     }
 }
