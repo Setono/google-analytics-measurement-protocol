67bf82f43f8b8bbec1c459bf2542eb3a865ca91d
Joachim Løvgaard <joachim@loevgaard.dk>
2023-03-21 10:37:45 +0100

    Add shipping info event

---
+++ src/Request/Body/Event/AddShippingInfoEvent.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\GoogleAnalyticsMeasurementProtocol\Request\Body\Event;
+
+use Setono\GoogleAnalyticsMeasurementProtocol\Request\Body\Event\Item\Item;
+use Setono\GoogleAnalyticsMeasurementProtocol\Request\Body\Event\Trait\CreatesEmpty;
+use Setono\GoogleAnalyticsMeasurementProtocol\Request\Body\Event\Trait\HasCoupon;
+use Setono\GoogleAnalyticsMeasurementProtocol\Request\Body\Event\Trait\HasCurrency;
+use Setono\GoogleAnalyticsMeasurementProtocol\Request\Body\Event\Trait\HasItems;
+use Setono\GoogleAnalyticsMeasurementProtocol\Request\Body\Event\Trait\HasValue;
+
+class AddShippingInfoEvent extends Event
+{
+    use CreatesEmpty;
+    use HasCurrency;
+    use HasValue;
+    use HasCoupon;
+    use HasItems;
+
+    protected ?string $shippingTier = null;
+
+    public function getEventName(): string
+    {
+        return 'add_shipping_info';
+    }
+
+    public function getShippingTier(): ?string
+    {
+        return $this->shippingTier;
+    }
+
+    public function setShippingTier(?string $shippingTier): self
+    {
+        $this->shippingTier = $shippingTier;
+
+        return $this;
+    }
+
+    protected function getParameters(): array
+    {
+        return [
+            'currency' => $this->currency,
+            'value' => $this->value,
+            'coupon' => $this->coupon,
+            'shipping_tier' => $this->shippingTier,
+            'items' => array_map(static fn (Item $item) => $item->getParameters(), $this->items),
+        ];
+    }
+}
